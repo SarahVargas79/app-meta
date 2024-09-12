68e47500b7ee9873240ca17b27f63a4722896725
SarahVargas79 <rafaelav4521@gmail.com>
2024-09-12 01:53:06 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts') // Extraindo uma função ou código...
+const { select, input, checkbox } = require('@inquirer/prompts') // Extraindo uma função ou código...
 
 let meta = {
     value: 'Comer ao menos uma fruta diariamante',
@@ -14,12 +14,46 @@ const cadastrarMeta = async () => {
         meta - Que for escrito vai ser colocado na meta
         lenght - Verifica se tem caractér, número de caractéres
         */
-    if (meta.lenght == 0) {
+    if (meta.length == 0) {
         console.log('A meta não pode ser vazia.')
         return
     }
     // push - Colocar para dentro
     metas.push({ value: meta, checked: false })
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: " Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar a etapa",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if (respostas.length == 0) {
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+
+
+    /*
+    Para cada executa a função
+    Lógica de marcar meta
+    */
+    respostas.forEach((resposta) => {
+        //find - cada uma, no caso metas
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    // Desmarcar meta
+    metas.forEach((m) => {
+        m.checked = false
+    })
+    console.log('Meta(s) marcadas como concluída(s)')
 }
 
 // Registra a função, iniciar função, começa a percorrer o while 
@@ -55,10 +89,10 @@ const start = async () => {
                 console.log(metas)
                 break
             case "listar":
-                console.log("Lista")
+                await listarMetas()
                 break
             case "logout":
-                console.log("Até a próxima")
+                console.log("Até a próxima!")
                 return
         }
     }
